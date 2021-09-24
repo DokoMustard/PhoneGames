67df251263365f1995ad359a4db1b19219a06467
Juho <aa4516@student.jamk.fi>
2021-09-24 10:21:30 +0300

    Edited color changer for objects. New script

---
+++ Assets/Scripts&Code/ObjectHit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+
+    static Color[] Colors = new Color[] { Color.red, Color.blue, Color.green, Color.black, Color.cyan };
+
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Cube" || other.gameObject.tag == "Sphere" || other.gameObject.tag == "Cylinder" || other.gameObject.tag == "Capsule")
+        {
+            Debug.Log(other.gameObject.name + " collides with " + name);
+            gameObject.tag = "Floor";
+            other.gameObject.GetComponent<MeshRenderer>().material.color = Colors[Random.Range(0, Colors.Length - 1)];
+        }
+    }
+
+}
